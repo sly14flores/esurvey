4ee2391194002c820e57ac4e0273d58583aa0473
Sly Flores <sly@christian.com.ph>
2020-08-30 15:49:27 +0800

    ...

--- public/conduct/js/sopa.js
+++ public/conduct/js/sopa.js
@@ -67513,7 +67513,7 @@ var render = function() {
         },
         [
           _c("iframe", {
-            staticStyle: { height: "100%", width: "100%" },
+            staticStyle: { height: "600px", width: "100%" },
             attrs: {
               src: "https://survey.launion.gov.ph/survey#/Ux6PqvC2UA",
               title: "One Kaprobinsiaan for A Stronger La Union"
