13ec624464865318655207272ded1503e3984983
Sly Flores <sly@christian.com.ph>
2020-08-30 15:32:53 +0800

    ...

--- resources/conduct/sopaHijack.vue
+++ resources/conduct/sopaHijack.vue
@@ -20,6 +20,9 @@
 	import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 	Vue.use(BootstrapVue)
 	Vue.use(IconsPlugin)
+	
+	import 'bootstrap/dist/css/bootstrap.css'
+	import 'bootstrap-vue/dist/bootstrap-vue.css'	
 
 	export default {
 	
