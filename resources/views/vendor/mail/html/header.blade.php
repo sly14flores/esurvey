a773e83b9b6b582107d88d3e719dd5adc47da2f3
Sly Flores <sly@christian.com.ph>
2020-11-09 09:53:47 +0800

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="https://survey.launion.gov.ph" style="display: inline-block;">
-<img src="https://esurvey.local/images/logo.png" class="logo" alt="{{ $slot }}">
+<img src="{{env('APP_URL').'/images/logo.png'}}" class="logo" alt="{{ $slot }}">
 </a>
 </td>
 </tr>
