b5fbbcf5ba32c6c7f4a4955b666fb2380d2acd3f
Sly Flores <sly@christian.com.ph>
2021-05-12 20:27:04 +0800

    Update ResetPasswordController.php

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Events\PasswordReset;
 
 use Illuminate\Support\Str;
 
