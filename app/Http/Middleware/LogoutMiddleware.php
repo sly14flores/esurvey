9d149b53a6f70947734fbb0cc76c80b23f31f908
Sly Flores <sly@christian.com.ph>
2021-05-12 19:58:16 +0800

    Update LogoutMiddleware.php

--- app/Http/Middleware/LogoutMiddleware.php
+++ app/Http/Middleware/LogoutMiddleware.php
@@ -23,9 +23,7 @@ class LogoutMiddleware
         if ($request->wantsJson()) {
 
             if ($request->header('Authorization') == "Bearer null") {
-                Auth::guard()->logout();
-                $request->session()->invalidate();
-                $request->session()->regenerateToken();            
+                Auth::guard()->logout();           
             }
 
         }
